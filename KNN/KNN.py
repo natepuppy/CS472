1b083080c57fba8ad334c17170118faf229cba95
Nathan Clark <nathanmclark23@gmail.com>
2020-03-19 22:24:53 -0600

    finished KNN

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -3,13 +3,19 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from tools.arff import Arff
-import math as math
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.decomposition import PCA
+
 
 class KNNClassifier(BaseEstimator,ClassifierMixin):
-    def __init__(self,columntype=[],weight_type='inverse_distance',k_value=3): ## add parameters here
+    def __init__(self,columntype=[],weight_type='inverse_distance',k_value=3,dist_metric=False,ranges=[]): ## add parameters here
         self.columntype = columntype #Note This won't be needed until part 5
         self.weight_type = weight_type
         self.k_value = k_value
+        self.dist_metric = dist_metric
+        self.ranges = ranges
 
     def fit(self,X,y):
         self.X = X
@@ -19,9 +25,9 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
     def predict(self, data):
         return_values = []
 
-        for i in range(len(data)):     # FIXME IS there a fatser way than this??
-            percent_completed = (i / len(data)) * 100
-            print(round(percent_completed, 4), "%")
+        for i in range(len(data)):     # FIXME IS there a faster way than this??
+            # percent_completed = (i / len(data)) * 100
+            # print(round(percent_completed, 4), "%")
 
             distances = []
             indexes = []
@@ -46,12 +52,11 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
                             distances[max_value_index] = dist
                             indexes[max_value_index] = j
                             outputs[max_value_index] = self.y[j]
-
-                # now that we have the k closest instances, find the correct output class, or regression value
-                if self.columntype[-1] != 'nominal':
-                    output = self.determine_regression_prediction(distances, indexes, outputs)
-                else:
-                    output = self.determine_nominal_class(distances, indexes, outputs)
+            # now that we have the k closest instances, find the correct output class, or regression value
+            if self.columntype[-1] != 'nominal':
+                output = self.determine_regression_prediction(distances, indexes, outputs)
+            else:
+                output = self.determine_nominal_class(distances, outputs)
             return_values.append(output)
         return return_values
 
@@ -65,38 +70,44 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
             else:
                 dist = (arr1[k] - arr2[k]) ** 2
             total += dist
+            # if arr1[k] == '?' or arr2[k] == '?':
+            #     total += 1
+            #     continue
+            #
+            # if self.columntype[k] == 'nominal':   # find distance for two nominal values  # FIXME This is an array
+            #     if arr1[k] != arr2[k]:
+            #         total = 1
+            # else:
+            #     if not self.dist_metric:
+            #         total += (arr1[k] - arr2[k]) ** 2
+            #     else:
+            #         total += np.abs((arr1[k] - arr2[k])) / self.ranges[k]
 
-        total += np.sqrt(total)
+        total = np.sqrt(total)
         if self.weight_type == 'inverse_distance':
             if total == 0:
                 return 1   # FIXME!!!
             total = 1 / (total ** 2)
         return total
 
-    def determine_nominal_class(self, distances, indexes, outputs):
-        out_classes = outputs
-        # out_classes = []
-        # for k in range(len(indexes)):
-        #     index = indexes[k]
-        #     out_class = self.y[index]
-        #     out_classes.append(out_class)
-
-        # counts = how many of that output class there are, value = all the distinct output classes
-        values, counts = np.unique(out_classes, return_counts=True)
-
-        if self.weight_type == 'inverse_distance':     # calculate the weighted votes   # FIXME TODO!  Probably broken here!
-            weighted_votes = []
-            for k in range(len(values)):   # for each type of out class
-                current_vote = 0
-                for l in range(len(out_classes)):   # for each one in the k instances
-                    if out_classes[l] == values[k]:
-                        current_vote += distances[l]
-                weighted_votes.append(current_vote)
-            ind = np.argmax(weighted_votes)
+    def determine_nominal_class(self, distances, outputs):
+        # if self.weight_type == 'inverse_distance':
+        #     ind = np.argmax(distances)
+        #     return outputs[ind]
+        if self.weight_type == 'inverse_distance':
+            vals = []
+            values, counts = np.unique(outputs, return_counts=True)
+            for k in range(len(values)):   # for each class
+                vals.append(0)
+                for a in range(len(distances)): # for each values
+                    if outputs[a] == values[k]:
+                        vals[k] += distances[a]
+            ind = np.argmax(vals)
             return values[ind]
-        else:  # get the most common output class
-            ind = np.argmax(counts)
-            return values[ind]
+
+        values, counts = np.unique(outputs, return_counts=True)
+        ind = np.argmax(counts)
+        return values[ind]
 
     def determine_regression_prediction(self, distances, indexes, outputs):
         numerator = 0
@@ -113,6 +124,7 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
         if self.columntype[-1] == 'nominal':
             total = 0
             for i in range(len(y)):
+                # print(int(predictions[i]))
                 if abs(y[i][0] - predictions[i]) < .0000000001:
                     total += 1
                 else:
@@ -121,23 +133,72 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
         else:
             total = 0
             for i in range(len(y)):
-                print(y[i][0], " -- ", predictions[i])
-                total += np.abs((y[i][0] - predictions[i]))
-            return total / len(y)
+                total += ((y[i][0] - predictions[i]) ** 2)
+            return (total / len(y))
 
+def wrapper(X, y):
+    num_features_to_drop = 0
+    clf = KNeighborsClassifier(n_neighbors=3, weights='distance')
+
+    for i in range(num_features_to_drop):
+        index_of_attr_to_delete = -1
+        lowest_score = 100
+        for j in range(len(X[0])):
+            X_copy = X.copy()
+            new_x = np.delete(X_copy, i, 1)
+            X1, X2, y1, y2 = train_test_split(new_x, y, test_size=0.30)
+            clf.fit(X1, y1.flatten())
+            score = clf.score(X2, y2.flatten())
+            if score < lowest_score:
+                lowest_score = score
+                index_of_attr_to_delete = j
+        X = np.delete(X, index_of_attr_to_delete, 1)
+
+    X1, X2, y1, y2 = train_test_split(X, y, test_size=0.30)
+    clf.fit(X1, y1.flatten())
+    score = clf.score(X2, y2.flatten())
+    return score
+
+def run_credit_approval():
+    # hyper-parameters
+    weight_type = 'inverse_distance'  # inverse_distance or distance   # make boolean
+    k_value = 3
+    normalization = False
+    dist_metric = True
+
+    # Get the file and Parse the data
+    file_train = "credit_approval.arff"
+    mat_train = Arff(file_train, label_count=1)
+    data_train = mat_train.data[:, 0:-1]
+    labels_train = mat_train.data[:, -1].reshape(-1, 1)
+    column_types = mat_train.attr_types
+
+    ranges_train = []
+    if normalization:
+        data_train, ranges_train = normalize(data_train, column_types)
+
+    X1, X2, y1, y2 = train_test_split(data_train, labels_train, test_size=0.10)
+
+    PClass = KNNClassifier(columntype=column_types, weight_type=weight_type, k_value=k_value, dist_metric=dist_metric,
+                           ranges=ranges_train)
+    PClass.fit(X1, y1)
+    score = PClass.score(X2, y2)
+    print(score)
 
 def normalize(X, column_types):
     num_attributes = len(X[0])
     num_instances = len(X)
-    col_max = X.max(axis=0)
-    col_min = X.min(axis=0)
+    col_max = np.nanmax(X, axis=0)
+    col_min = np.nanmin(X, axis=0)
+    ranges = col_max - col_min
 
     for i in range(num_attributes):
         if column_types[i] == 'nominal':
             continue
         for j in range(num_instances):
             X[j][i] = (X[j][i] - col_min[i]) / (col_max[i] - col_min[i])
-    return X
+    return X, ranges
+
 
 
 def start():
@@ -152,14 +213,21 @@ def start():
         "seismic-bumps_train",  # 6
         "telescope_test",       # 7
         "telescope_train",      # 8
+        "ionosphere"           # 9
     ]
 
+    # Which type to run
+    run_normal = False
+    run_sk_learn = False
+    run_sk_with_PCA_and_wrapper = True
+
     # hyper-parameters
-    test_index = 2
-    train_index = 1
+    test_index = 9
+    train_index = 9
     weight_type = 'inverse_distance'   # inverse_distance or distance
     k_value = 3
     normalization = False
+    dist_metric = False
 
     # Get the file and Parse the data
     file_test = arff_files[test_index] + ".arff"
@@ -173,130 +241,61 @@ def start():
     labels_train = mat_train.data[:,-1].reshape(-1,1)
     column_types = mat_test.attr_types
 
+    ranges_train = []
     if normalization:
-        data_test = normalize(data_test, column_types)
-        data_train = normalize(data_train, column_types)
+        data_test, ranges_test = normalize(data_test, column_types)
+        data_train, ranges_train = normalize(data_train, column_types)
 
-    # Train and Score
-    PClass = KNNClassifier(columntype=column_types, weight_type=weight_type, k_value=k_value)
-    PClass.fit(data_train, labels_train)
-    score = PClass.score(data_test, labels_test)
-    print(score)
+    # Run the different types
+    if run_normal:
+        PClass = KNNClassifier(columntype=column_types, weight_type=weight_type, k_value=k_value, dist_metric=dist_metric, ranges=ranges_train)
+        PClass.fit(data_train, labels_train)
+        score = PClass.score(data_test, labels_test)
+        print(score)
+
+    if run_sk_learn:
+        clf = KNeighborsClassifier(n_neighbors=3, weights='distance')
+        # clf = KNeighborsRegressor(n_neighbors=3)
+        clf.fit(data_train, labels_train.flatten())
+        score = clf.score(data_test, labels_test.flatten())
+        print(score)
+
+    if run_sk_with_PCA_and_wrapper:
+        pca = PCA(n_components=33)  # 34 attributes total
+        result_data = pca.fit_transform(data_train)
+        score = wrapper(result_data, labels_train)
+        print(score)
 
 start()
 
 
+# sklearn.neighbors.KNeighborsClassifier
+
+
+# if run_all:
+#     k_values = [1, 3, 5, 7, 9, 11, 13, 15]
+#     for i in range(len(k_values)):
+#         PClass = KNNClassifier(columntype=column_types, weight_type=weight_type, k_value=k_values[i],
+#                                dist_metric=dist_metric, ranges=ranges_train)
+#         PClass.fit(data_train, labels_train)
+#         score = PClass.score(data_test, labels_test)
+#         print(k_values[i], " -- ", score)
+#
+#     weight_type = "regular"
+#     k_values = [1, 3, 5, 7, 9, 11, 13, 15]
+#     for i in range(len(k_values)):
+#         PClass = KNNClassifier(columntype=column_types, weight_type=weight_type, k_value=k_values[i],
+#                                dist_metric=dist_metric, ranges=ranges_train)
+#         PClass.fit(data_train, labels_train)
+#         score = PClass.score(data_test, labels_test)
+#         print(k_values[i], " -- ", score)
+
 # TODO
-# How to handle real outputs
-# is normalization only for the input features? -- yes i think
 # normalize test and train? -- yes i think
 # inverse distance with a dist of zero
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 """
 git add .
-git commit -m "Update for KNN"
+git commit -m "finished KNN"
 git push
 """
-
-
-
-
-
-"""
-Args:
-    columntype for each column tells you if continues[real] or if nominal[categoritcal].
-    weight_type: inverse_distance voting or if non distance weighting. Options = ["no_weight","inverse_distance"]
-"""
-
-# out_classes = []
-# for k in range(len(indexes)):
-#     index = indexes[k]
-#     out_class = self.y[index]
-#     out_classes.append(out_class)
-# values, counts = np.unique(out_classes, return_counts=True)
-# ind = np.argmax(counts)
-# return_values.append(values[ind])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# if self.weight_type == 'inverse_distance':
-#     # dist = 1 / ((np.sqrt(np.sum((data[i] - self.X[j]) ** 2))) ** 2)
-# else:
-#     # dist = np.sqrt(np.sum((data[i] - self.X[j]) ** 2))
-
-
-# def euclidean_distance_2(self, x1, y1, x2, y2):
-#     math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
-
-
-#     if self.weight_type == 'inverse_distance':
-#         distances = [np.inf for j in range(self.k_value)]
-#     else:
-#         distances = [np.NINF for j in range(self.k_value)]
-#     indexes = [0 for j in range(self.k_value)]
-#
-#     for j in range(len(self.X)):  # each instance in the train set
-#         dist = self.calculate_distance(data[i], self.X[j])
-#
-#         if self.weight_type == 'inverse_distance':
-#             min_value_index = int(np.argmin(distances))
-#             if distances[min_value_index] < dist:
-#                 distances[min_value_index] = dist
-#                 indexes[min_value_index] = j
-#         else:
-#             max_value_index = int(np.argmax(distances))
-#             if distances[max_value_index] > dist:
-#                 distances[max_value_index] = dist
-#                 indexes[max_value_index] = j
-#
-#             # now that we have the k closest instances, find the correct output class, or regression value
-#         if self.columntype[-1] != 'nominal':
-#             output = self.determine_regression_prediction(distances, indexes)
-#         else:
-#             output = self.determine_nominal_class(distances, indexes)
-#     return_values.append(output)
-# return return_values
-
-
-
-
-
